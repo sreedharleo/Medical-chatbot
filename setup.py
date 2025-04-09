ab17844150c4ae7b47449257fb488f89ae15a5d2
sreedharleo <rsreedhar103@gmail.com>
2025-04-09 21:50:46 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Gen AI Project",
+    version= '0.0.0',
+    author= 'Sreedhar',
+    author_email= 'rsreedhar103@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+    
+)
